8ad679a18b28c04d75a27736d1916659cb81aeb6
Iago Nobre <iagonobre22@gmail.com>
2024-10-08 15:57:36 -0300

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let drawing = false;
+
+function startDrawing(e) {
+    drawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.offsetX, e.offsetY); 
+}
+
+function draw(e) {
+    if (!drawing) return;
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.strokeStyle = 'black'; 
+    ctx.lineWidth = 1;
+    ctx.stroke();
+}
+
+function stopDrawing() {
+    drawing = false;
+    ctx.closePath(); 
+}
+
+
+const clearButton = document.getElementById('clearCanvas');
+
+clearButton.addEventListener('click', () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('mousemove', draw); 
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mouseout', stopDrawing);
